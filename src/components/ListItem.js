d10b96c4f8ed12056423bddce74ad8f706679fad
James Youn <jamesyoun710@gmail.com>
2017-04-23 23:45:44 -0700

    Add animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation } from 'react-native'
 import { connect } from 'react-redux' // can be used to get action state but also to generate action creators
 import { CardSection } from './common'
 import * as actions from '../actions'
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring()
+  }
+
   renderDescription() {
     const {library, expanded} = this.props
 
     if (expanded) {
       return (
-        <Text>{library.description}</Text>
+        <CardSection>
+          <Text>{library.description}</Text>
+        </CardSection>
       )
     }
   }
